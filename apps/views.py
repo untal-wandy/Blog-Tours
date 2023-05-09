d67c7d5bda8b6a1401746c874f0da5946d2574f5
Wandy Oli <wandy.oli@icloud.com>
2023-05-09 14:13:16 -0400

    Apps correo listo

--- apps/views.py
+++ apps/views.py
@@ -1,36 +1,71 @@
-from django.shortcuts import render,redirect
-from django.core.mail import EmailMessage
-from django.conf import settings
+from email.mime.text import MIMEText
+import smtplib
+from email.mime.multipart import MIMEMultipart
+# from email import MIMEText
+from smtplib import SMTP
 
+from django.conf import settings
+from django.core.mail import EmailMessage, send_mail
+from django.shortcuts import redirect, render
 from django.template.loader import render_to_string
+
 
 # Create your views here.
 def index(request):
 
     if request.method ==   'POST':
-        name = request.POST['name']
-        email = request.POST['email']
-        subject = request.POST['asunto']
-        mensaje = request.POST['mensaje']
+        name = request.POST.get('name', ' ')
+        email = request.POST.get('email', ' ')
+        subject = request.POST.get('subject', ' ')
+        mensaje = request.POST.get('mensaje', ' ')
 
+
+        mailServer = smtplib.SMTP(settings.EMAIL_HOST, settings.EMAIL_PORT)
+        mailServer.ehlo()
+        print(mailServer.ehlo())
+        mailServer.starttls()
+        mailServer.ehlo()
+        mailServer.login(settings.EMAIL_USER, settings.EMAIL_HOTS_PASSWORD)
+        print("Conectado")
+
+
+        mensaje = MIMEText("""Este es el mensaje
+        de las narices""")
+        mensaje['From']= settings.EMAIL_HOST
+        mensaje['To']= 'olivares.wan@gmail.com'
+        mensaje['Subject']="Tienes un correo"
+
+        # Envio del mensaje
+        mailServer.sendmail(settings.EMAIL_HOST,
+                        "olivares.wan@gmail.com",
+                        mensaje.as_string())g
+        print('Correo enviado')
+
+        # send = send_mail (
+        #     name, mensaje, email,
+        #     ['olivares.wan@gmail.com'],
+        #     fail_silently=False,
+        # )
+
+        # try:
+        #     server = smtplib.SMTP('smtp.gmail.com', 587)
+        #     server.ehlo()
+        # except:
+        #     print('Something went wrong...')
 
         template = {
-
             'name': name,
             'email': email,
-            'message':mensaje,
-        }
+            'message':mensaje,  }
 
-        
+        # email = EmailMessage( 
+        #     'Mensaje recivido',
+        #     'Mensaje recivido por: Wandy Olivares: '.format(name, email, mensaje),
+        #     email,
+        #     ['olivares.wan@gmail.com']
+        # )
+        # email.send()
 
-        email = EmailMessage(
-            subject,
-            template,
-            settings.EMAIL_HOST_USER,
-            ['olivares.wan@gmail.com']
-        )
-        email.fail_silently = False
-        email.send()
         # return redirect('/contact/',template)
 
         return render(request, 'apps/contact.html', template)
